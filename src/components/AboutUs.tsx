4932437fb6c17549c929986a5db8be2c7d67c00a
Liang <18189305423@163.com>
2026-07-21 17:33:32 +0800

    更新关于我们组件的图片源，修改为关于我们图片以提升加载效果

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -29,7 +29,7 @@ export default function AboutUs({ onBack }: { onBack: () => void }) {
               <div className="overflow-hidden rounded-2xl">
                 {/* 宽高比改为 4:3 */}
                 <img 
-                  src="/public/linimg/ss(12).webp" 
+                  src="/linimg/aboutus.jpg" 
                   alt="关于我们图片" 
                   className="aspect-[4/3] w-full object-cover" 
                 />
